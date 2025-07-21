eb597eaa2a4f51f7f41383fdd267b0a5e1938dd8
Tdavide04 <tdavide833@gmail.com>
2025-07-21 09:20:14 +0200

    Fix comments

--- Java/src/leetCode/Problem_703.java
+++ Java/src/leetCode/Problem_703.java
@@ -5,8 +5,8 @@ import java.util.PriorityQueue;
 public class Problem_703 {
 
 	/*
-	 * 703. Kth Largest Element in a Stream You are part of a university admissions
-	 * office and need to keep track of the kth highest test score from applicants
+	 * 703. Kth Largest Element in a Stream 
+	 * You are part of a university admissions office and need to keep track of the kth highest test score from applicants
 	 * in real-time. This helps to determine cut-off marks for interviews and
 	 * admissions dynamically as new applicants submit their scores.
 	 * 
